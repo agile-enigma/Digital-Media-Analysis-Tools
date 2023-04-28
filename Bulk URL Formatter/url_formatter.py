5dc84669f164ed58d3d59165021d642715640e4c
agile-enigma <110642777+agile-enigma@users.noreply.github.com>
2023-04-28 01:57:59 -0400

    Update url_formatter.py

--- Bulk URL Formatter/url_formatter.py
+++ Bulk URL Formatter/url_formatter.py
@@ -8,7 +8,7 @@ class formatter:
     def __init__(self, raw_links):
         self.raw_links = raw_links
 
-    def format(self):
+    def clean(self):
         self.headers = {"user-agent" : "Mozilla/5.0 (Windows NT 10.0; Win64; x64)\
         AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.5615.121 Safari/537.36"}
 
@@ -356,7 +356,7 @@ if __name__ == '__main__':
         for line in lines:
             raw_links.append(line.replace("\n", ""))
 
-    formatted_links = formatter(raw_links).format()
+    formatted_links = formatter(raw_links).clean()
 
     with open(identifier + '_cleaned_links.txt', 'w') as file:
         for link in formatted_links:
